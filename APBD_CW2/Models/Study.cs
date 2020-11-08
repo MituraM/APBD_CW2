2a8bbf0aba6d08dce16494908cf086c078c0e9e4
MaciejMitura <m.mitura@be-tse.com>
2020-11-08 21:27:29 +0100

    cw 2 commit

---
+++ APBD_CW2/Models/Study.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace APBD_CW2.Models
+{
+    public class Study
+    {
+        [XmlElement(elementName: "name")]
+        public string name { get; set; }
+
+        [XmlElement(elementName: "mode")]
+        public string mode { get; set; }
+    }
+}
